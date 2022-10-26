66f2a9312df5345bae34275745d43c7e737b3820
Golden-Man <Golden-Man@users.noreply.github.com>
2022-10-26 04:20:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array)
+{
+    if(array.find((val) => val.result === 'W'))
+    {
+        return array.find((val) => val.result === 'W').year
+    }
+}
